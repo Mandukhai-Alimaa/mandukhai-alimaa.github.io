0f0b932dc83536ff9f82d4d857b9e46158099e1e
MANDY Alimaa <alimaa@wisc.edu>
2025-01-02 23:44:10 -0600

    update the resume and projects

--- src/components/data/projectsData.js
+++ src/components/data/projectsData.js
@@ -4,19 +4,25 @@ const projectsData = [
         key: 1,
         imageSrc: `${process.env.PUBLIC_URL}/scheduler.png`,
         overlayText: "Scheduler Web Application",
-        paragraph: "Led the development of a scheduling application for outpatient respiratory telehealth services, which is actively used by over 100 users daily. I implemented features that streamlined the scheduling process, reducing the time spent scheduling each patient by over 80%, while maintaining a 99% uptime. My contributions significantly enhanced the user experience, resulting in a 40% increase in customer satisfaction."
+        paragraph: "The Scheduler application modernized the outpatient respiratory telehealth scheduling process by transitioning from Excel-based methods to an integrated web-based solution. Used daily by over 100 users for extended periods, the application maintains 99% uptime and has transformed patient scheduling by reducing time per patient by 80%. Its real-time appointment tracking and automated reminders decreased missed appointments by 20%, while improved staff coordination boosted efficiency by 30%. By simplifying workflows and increasing customer satisfaction by 40%, the application has become an indispensable tool for telehealth services."
     },
     {
         key: 2,
         imageSrc: `${process.env.PUBLIC_URL}/adam.png`,
         overlayText: "CoreLink Web Application",
-        paragraph: "Played a key role in developing the CoreLink application, designed for users to manage shifts and handle incoming calls from inpatient hospitals. I focused on optimizing system performance by reducing call wait times by 50% and server load by 80%. By implementing efficient coding practices and real-time data handling, I significantly improved user experience and system reliability, ensuring a smoother workflow for all users."
+        paragraph: "The CoreLink application revolutionized call handling for inpatient hospitals by enabling users to seamlessly sign up for shifts and manage incoming calls. By integrating a WebSocket-based system to replace a legacy polling mechanism, it drastically improved real-time data updates while reducing unnecessary server requests by 75%. This optimization decreased call wait times by 50% and reduced server load by 80%, significantly enhancing performance and user experience. Designed to handle high volumes of concurrent users, the platform streamlined hospital operations and contributed to a more efficient and responsive healthcare environment."
     },
     {
         key: 3,
-        imageSrc: `${process.env.PUBLIC_URL}/personal-website.png`,
-        overlayText: "Personal Portfolio Website",
-        paragraph: "This is platform to showcase my professional and academic journey. It includes my resume, detailed descriptions of coursework, a portfolio of projects, and recommendation letters from past employers. The site is designed to be user-friendly and highlights my full-stack development skills, as well as my passion for continuous learning and professional growth. This project reflects my commitment to transparency, allowing people access my qualifications and achievements in one place."
+        imageSrc: `${process.env.PUBLIC_URL}/wisconsinShell.webp`,
+        overlayText: "Unix Shell",
+        paragraph: "The Wisconsin Shell (wsh) is a Unix shell implementation designed to explore command-line interpreter functionality, offering features like command execution, redirections, environment and shell variable management, command history tracking, and built-in commands (exit, cd, export, local, vars, history), supporting both interactive and batch modes with additional comment handling."
+    },
+    {
+        key: 4,
+        imageSrc: `${process.env.PUBLIC_URL}/fileSystem.webp`,
+        overlayText: "FUSE-based Unix filesystem",
+        paragraph: "This project involves implementing a FUSE-based Unix filesystem in C with RAID 0 and RAID 1 capabilities, supporting basic file and directory operations, metadata management, and error handling, while adhering to specific on-disk data structures and RAID configurations."
     },
 ];
 
--- src/components/data/recommendationData.js
+++ src/components/data/recommendationData.js
@@ -4,11 +4,11 @@ const recommendationData = [
         src: `${process.env.PUBLIC_URL}/JustinRecommendation.png`,
         alt: 'Chief Strategic Officer Recommendation'
     },
-    // {
-    //     key: 2,
-    //     src: `${process.env.PUBLIC_URL}/UWMadison-logo.jpg`,
-    //     alt: 'Senior Software Architect Recommendation'
-    // },
+    {
+        key: 2,
+        src: `${process.env.PUBLIC_URL}/TimRecommendation.png`,
+        alt: 'Senior Software Architect Recommendation'
+    },
 ];
 
 export default recommendationData;

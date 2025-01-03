235643976e77eb4fdf994a0adbcf4eac2b978e15
MANDY Alimaa <alimaa@wisc.edu>
2025-01-03 00:13:43 -0600

    Update on project desc

--- src/components/data/projectsData.js
+++ src/components/data/projectsData.js
@@ -4,13 +4,13 @@ const projectsData = [
         key: 1,
         imageSrc: `${process.env.PUBLIC_URL}/scheduler.png`,
         overlayText: "Scheduler Web Application",
-        paragraph: "The Scheduler application modernized the outpatient respiratory telehealth scheduling process by transitioning from Excel-based methods to an integrated web-based solution. Used daily by over 100 users for extended periods, the application maintains 99% uptime and has transformed patient scheduling by reducing time per patient by 80%. Its real-time appointment tracking and automated reminders decreased missed appointments by 20%, while improved staff coordination boosted efficiency by 30%. By simplifying workflows and increasing customer satisfaction by 40%, the application has become an indispensable tool for telehealth services."
+        paragraph: "The Scheduler application modernized the outpatient respiratory telehealth scheduling process by transitioning from Excel-based methods to an integrated web-based solution. Used daily by over 100 users for extended periods, the application maintains 99% uptime and has transformed patient scheduling by reducing time per patient by 80%. Its real-time appointment tracking and automated reminders decreased missed appointments by 20%, while improved staff coordination boosted efficiency by 30%. By simplifying workflows and increasing customer satisfaction, this has become an important tool."
     },
     {
         key: 2,
         imageSrc: `${process.env.PUBLIC_URL}/adam.png`,
         overlayText: "CoreLink Web Application",
-        paragraph: "The CoreLink application revolutionized call handling for inpatient hospitals by enabling users to seamlessly sign up for shifts and manage incoming calls. By integrating a WebSocket-based system to replace a legacy polling mechanism, it drastically improved real-time data updates while reducing unnecessary server requests by 75%. This optimization decreased call wait times by 50% and reduced server load by 80%, significantly enhancing performance and user experience. Designed to handle high volumes of concurrent users, the platform streamlined hospital operations and contributed to a more efficient and responsive healthcare environment."
+        paragraph: "The CoreLink application revolutionized call handling for inpatient hospitals by enabling users to seamlessly sign up for shifts and manage incoming calls. By integrating a WebSocket-based system to replace a legacy polling mechanism, it drastically improved real-time data updates while reducing unnecessary server requests by 75%. This optimization decreased call wait times by 50% and reduced server load by 80%, significantly enhancing performance and user experience."
     },
     {
         key: 3,
